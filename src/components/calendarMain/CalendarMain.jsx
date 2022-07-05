594518de1e807ddb053e7dc0139429a8d3ed9e21
addy <adil_15-05@mail.ru>
2022-07-05 13:31:20 +0600

    feat: create main

---
+++ src/components/calendarMain/CalendarMain.jsx
@@ -0,0 +1,98 @@
+import "./calendarMain.css";
+
+const defaultSelectors = {
+  years: [2020, 2021, 2022, 2023, 2024, 2025, 2026, 2027, 2028, 2029, 2030],
+  monthNames: [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ],
+  weekDayNames: ["", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+};
+
+const CalendarMain = () => {
+  const { years, monthNames, weekDayNames } = defaultSelectors;
+  const monthData = [
+    [
+      "",
+      new Date(),
+      new Date(),
+      new Date(),
+      new Date(),
+      new Date(),
+      new Date(),
+      new Date(),
+    ],
+  ];
+  const matrix = {
+    blocks: [...Array.from(Array(24).keys())].map((i) => [...Array(8).keys()]),
+  };
+  return (
+    <div className="main">
+      <div className="calendarSelector">
+        <table className="dayOfWeek">
+          <thead className="dayName">
+            <tr>
+              {weekDayNames.map((name) => (
+                <th key={name}>{name}</th>
+              ))}
+            </tr>
+          </thead>
+          <tbody className="dayNumber">
+            {monthData.map((week, index) => (
+              <tr key={index} className="week">
+                {week.map((date, index) =>
+                  date ? (
+                    <td key={index}>{date.getDate()} </td>
+                  ) : (
+                    <td key={index} />
+                  )
+                )}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <div className="slider">
+          <button>{"<"}</button>
+          <div className="selects">
+            <select id="month">
+              {monthNames.map((name, index) => (
+                <option key={name} value={index}>
+                  {name}
+                </option>
+              ))}
+            </select>
+            <select id="year">
+              {years.map((year) => (
+                <option key={year}>{year}</option>
+              ))}
+            </select>
+          </div>
+          <button>{">"}</button>
+        </div>
+      </div>
+      <table className="calendarMatrix">
+        <tbody>
+          {matrix.blocks.map((time) => (
+            <tr>
+              {time.map((day) => (
+                <th>{day}</th>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default CalendarMain;
